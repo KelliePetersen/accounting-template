f605c3c8c5ca6b8ebd1483b6b9e6fb90b1f09597
KelliePetersen <kelliepetersen@outlook.com>
2020-07-08 19:33:52 +1000

    create Footer component

---
+++ src/components/footer/footer.js
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types"
+import React from 'react'
+import { Link } from "gatsby"
+import styles from "./footer.module.scss"
+
+const Footer = ({ siteTitle }) => (
+  <footer>
+    © Copyright {siteTitle} {new Date().getFullYear()}. All rights reserved.
+  </footer>
+)
+
+Footer.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Footer.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Footer
