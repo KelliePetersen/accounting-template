071d71e1476e5ce03ce76db812a1c4b3ffc8ea5e
KelliePetersen <kelliepetersen@outlook.com>
2020-07-09 10:12:18 +1000

    add background image to hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -2,10 +2,12 @@ import PropTypes from "prop-types"
 import React from "react"
 import { Link } from "gatsby"
 import styles from "./hero.module.scss"
+import Image from "./image"
 
 const Hero = () => {
   return (
     <div className={styles.hero}>
+      <Image className={styles.image} />
       <div className={styles.container}>
         <h1 className={styles.heading}>This be where our super amazing title goes.</h1>
         <Link className={styles.button} to="#services">LEARN MORE</Link>
--- src/components/image.js
+++ src/components/image.js
@@ -16,9 +16,9 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+      heroImage: file(relativePath: { eq: "hero.png" }) {
         childImageSharp {
-          fluid(maxWidth: 300) {
+          fluid(maxWidth: 1920, quality: 90) {
             ...GatsbyImageSharpFluid
           }
         }
@@ -26,7 +26,7 @@ const Image = () => {
     }
   `)
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+  return <Img fluid={data.heroImage.childImageSharp.fluid} />
 }
 
 export default Image
