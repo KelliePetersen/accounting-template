ca0471cdac554b75845ea1a684358e28717f9a13
KelliePetersen <kelliepetersen@outlook.com>
2020-07-09 11:25:10 +1000

    style grid in about section

--- src/components/about.js
+++ src/components/about.js
@@ -2,17 +2,21 @@ import React from 'react'
 import styles from "./about.module.scss"
 
 const About = () => (
-    <section className={styles.about}>
+    <section className={styles.about} id="about">
       <div>
-        <h2>We've been in this game for 40 years.</h2>
+        <h2 className={styles.header}>We've been in this game for 40 years.</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam neque lorem, vulputate vitae ullamcorper
           vitae, mattis finibus enim. Sed blandit neque leo, eu facilisis libero mollis ut. </p>
       </div>
       <div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div className={styles.grid}>
+          <div></div>
+          <div></div>
+        </div>
+        <div className={`${styles.grid} ${styles.grid2}`}>
+          <div></div>
+          <div></div>
+        </div>
       </div>
     </section>
   )
