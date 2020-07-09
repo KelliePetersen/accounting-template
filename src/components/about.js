499ee53b602df38e7bbbb1cc46dd4784d61438f8
KelliePetersen <kelliepetersen@outlook.com>
2020-07-09 10:53:05 +1000

    create about section component

---
+++ src/components/about.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from "./about.module.scss"
+
+const About = () => (
+    <section className={styles.about}>
+      <div>
+        <h2>We've been in this game for 40 years.</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam neque lorem, vulputate vitae ullamcorper
+          vitae, mattis finibus enim. Sed blandit neque leo, eu facilisis libero mollis ut. </p>
+      </div>
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </section>
+  )
+
+export default About
