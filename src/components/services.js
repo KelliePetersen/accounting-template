e080c5deb8c89fe164438bd29e7f7b677bebda4b
KelliePetersen <kelliepetersen@outlook.com>
2020-07-09 17:32:36 +1000

    add empty services section component

---
+++ src/components/services.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./about.module.scss"
+
+const Services = () => {
+  return (
+    <section>
+      
+    </section>
+  )
+}
+
+export default Services
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 
 import Hero from "../components/hero"
 import About from "../components/about"
+import Services from "../components/services"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -10,6 +11,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <Hero />
     <About />
+    <Services />
   </Layout>
 )
 
