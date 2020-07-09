d9118b9c2d2680ec65dd94edd3903b096bf57918
KelliePetersen <kelliepetersen@outlook.com>
2020-07-09 17:36:54 +1000

    add html code to component

--- src/components/services.js
+++ src/components/services.js
@@ -1,10 +1,30 @@
 import React from 'react'
-import styles from "./about.module.scss"
+import styles from "./services.module.scss"
 
 const Services = () => {
   return (
-    <section>
-      
+    <section className={styles.services}>
+      <h2 className={styles.heading}>Our Services.</h2>
+      <div className={styles.grid}>
+        <div>
+          <div className={styles.icon}></div>
+          <h4 className={styles.subheading}>Accounting</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam neque lorem, 
+            vulputate vitae ullamcorper vitae, mattis finibus enim. </p>
+        </div>
+        <div>
+          <div className={styles.icon}></div>
+          <h4 className={styles.subheading}>Auditing</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam neque lorem, 
+            vulputate vitae ullamcorper vitae, mattis finibus enim. </p>
+        </div>
+        <div>
+          <div className={styles.icon}></div>
+          <h4 className={styles.subheading}>Advisors</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam neque lorem, 
+            vulputate vitae ullamcorper vitae, mattis finibus enim. </p>
+        </div>
+      </div>
     </section>
   )
 }
