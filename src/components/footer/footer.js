30591d68981ee77d0ffda1a709d2d9d891e0f69a
KelliePetersen <kelliepetersen@outlook.com>
2020-07-09 18:16:12 +1000

    add html code to footer component

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,7 +5,24 @@ import styles from "./footer.module.scss"
 
 const Footer = ({ siteTitle }) => (
   <footer className={styles.footer}>
-    © Copyright {siteTitle} {new Date().getFullYear()}. All rights reserved.
+    <div styles className={styles.leftcontainer}>
+      <h4>Heron<span>.AU</span></h4>
+      <p className={styles.copyright}>© Copyright {siteTitle} {new Date().getFullYear()}. All rights reserved.</p>
+    </div>
+    <div className={styles.rightcontainer}>
+      <nav className={styles.nav}>
+        <ul className={styles.list}>
+          <li><Link to="/#about">About</Link></li>
+          <li><Link to="/#services">Services</Link></li>
+          <li><Link to="/#features">Features</Link></li>
+          <li><Link to="/#location">Location</Link></li>
+          <li><Link to="/#contact">Contact</Link></li>
+        </ul>
+      </nav>
+      <p className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam neque lorem, 
+        vulputate vitae ullamcorper vitae, mattis finibus enim.</p>
+      <button className={styles.topbutton}>&uarr;</button>
+    </div>
   </footer>
 )
 
