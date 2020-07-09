c82435522c9f30ea36a122afec3e7e7e14cfe300
KelliePetersen <kelliepetersen@outlook.com>
2020-07-09 18:02:28 +1000

    add html code to slideshow component

--- src/components/slideshow.js
+++ src/components/slideshow.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import styles from "./slideshow.module.scss"
+import Image from "./image"
 
-const Slideshow = () => {
-  return (
-    <div>
-      
+
+const Slideshow = () => (
+  <div className={styles.slideshow}>
+    <div className={styles.container}>
+      <Image />
     </div>
-  )
-}
+    <div className={styles.nav}>
+      <div className={styles.leftarrow}>&larr;</div>
+      <div className={styles.rightarrow}>&rarr;</div>
+    </div>
+  </div>
+)
 
 export default Slideshow
