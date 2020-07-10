0b5925c73a0b69efcce6764466c4053a2730aa85
KelliePetersen <kelliepetersen@outlook.com>
2020-07-10 13:28:44 +1000

    tweak contactForm input margins, and make a11y-compliant

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -3,18 +3,15 @@ import styles from './contactForm.module.scss'
 
 const ContactForm = () => (
   <form className={styles.form} method="post">
-    <div>
-      <label>Full name</label>
-      <input className={styles.input} type="text" name="name" required/>
-    </div>
-    <div>
-      <label>Email</label>
-      <input className={styles.input} type="email" name="email" required/>
-    </div>
-    <div>
-      <label>Message</label>
-      <textarea className={styles.input} name="message" rows="5"></textarea>
-    </div>
+    <label htmlFor="name">Full name
+      <input className={styles.input} type="text" name="name" id="name" required/>
+    </label>
+    <label htmlFor="email">Email
+      <input className={styles.input} type="email" name="email" id="email" required/>
+    </label>
+    <label htmlFor="message">Message
+      <textarea className={styles.input} name="message" id="message" rows="5"></textarea>
+    </label>
     <button type="submit" className={styles.button}>Send message</button>
   </form>
 )
