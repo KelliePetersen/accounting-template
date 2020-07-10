fa1ac4627d04161704a62af5c51a32f603b62036
KelliePetersen <kelliepetersen@outlook.com>
2020-07-10 12:44:48 +1000

    add contact section component

---
+++ src/components/contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ContactForm from '../components/ContactForm'
+import styles from './contact.module.scss'
+
+const Contact = () => (
+  <section className={styles.contact} id="contact">
+      <div className={styles.wrapper}>
+          <h2 className={styles.heading}>Ready? Let's work together.</h2>
+          <ContactForm></ContactForm>
+      </div>
+    </section>
+)
+
+export default Contact
