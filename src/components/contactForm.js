4ce5c03378e392def63f7de1743be8e82047320a
KelliePetersen <kelliepetersen@outlook.com>
2020-07-10 13:35:44 +1000

    add hover and focus effects to contactForm input and submit button

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -10,7 +10,7 @@ const ContactForm = () => (
       <input className={styles.input} type="email" name="email" id="email" required/>
     </label>
     <label htmlFor="message">Message
-      <textarea className={styles.input} name="message" id="message" rows="5"></textarea>
+      <textarea className={styles.input} name="message" id="message" rows="5" required></textarea>
     </label>
     <button type="submit" className={styles.button}>Send message</button>
   </form>
