5351c72bb0514bfb1f689ceeaa1b2304280666ba
KelliePetersen <kelliepetersen@outlook.com>
2020-07-10 12:45:00 +1000

    add contactForm component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from "react"
 
+import SEO from "../components/seo"
+import Layout from "../components/layout"
 import Hero from "../components/hero"
 import About from "../components/about"
 import Services from "../components/services"
 import Features from "../components/features"
 import Slideshow from "../components/slideshow"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Contact from '../components/Contact'
+
 
 const IndexPage = () => (
   <Layout>
@@ -16,6 +18,7 @@ const IndexPage = () => (
     <Services />
     <Features />
     <Slideshow />
+    <Contact />
   </Layout>
 )
 
