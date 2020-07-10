bc17682bfc9ea96db0eef2b5fff0f05d78e4cee6
KelliePetersen <kelliepetersen@outlook.com>
2020-07-10 16:59:52 +1000

    make footer component responsive

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,24 +5,26 @@ import styles from "./footer.module.scss"
 
 const Footer = ({ siteTitle }) => (
   <footer className={styles.footer}>
-    <div styles className={styles.leftcontainer}>
-      <Link className={styles.logo} to="#">Heron<span className={styles.span}>.AU</span></Link>
-      <p className={styles.copyright}>© {siteTitle} {new Date().getFullYear()}.</p>
+    <div className={styles.wrapper}>
+      <div styles className={styles.leftcontainer}>
+        <Link className={styles.logo} to="#">Heron<span className={styles.span}>.AU</span></Link>
+      </div>
+      <div className={styles.rightcontainer}>
+        <nav className={styles.nav}>
+          <ul className={styles.list}>
+            <li><Link to="/#about">About</Link></li>
+            <li><Link to="/#services">Services</Link></li>
+            <li><Link to="/#features">Features</Link></li>
+            <li><Link to="/#location">Location</Link></li>
+            <li><Link to="/#contact">Contact</Link></li>
+          </ul>
+        </nav>
+        <p className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam neque lorem, 
+          vulputate mattis finibus enim.</p>
+        <Link className={styles.toplink} to="#">&uarr;</Link>
+      </div>
     </div>
-    <div className={styles.rightcontainer}>
-      <nav className={styles.nav}>
-        <ul className={styles.list}>
-          <li><Link to="/#about">About</Link></li>
-          <li><Link to="/#services">Services</Link></li>
-          <li><Link to="/#features">Features</Link></li>
-          <li><Link to="/#location">Location</Link></li>
-          <li><Link to="/#contact">Contact</Link></li>
-        </ul>
-      </nav>
-      <p className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam neque lorem, 
-        vulputate mattis finibus enim.</p>
-      <Link className={styles.toplink} to="#">&uarr;</Link>
-    </div>
+    <p className={styles.copyright}>© {siteTitle} {new Date().getFullYear()}.</p>
   </footer>
 )
 
