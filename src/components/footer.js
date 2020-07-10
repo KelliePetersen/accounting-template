9fc1b80389e10c96a2c5894332e192216c897d70
KelliePetersen <kelliepetersen@outlook.com>
2020-07-10 17:36:46 +1000

    update footer nav links

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,10 +12,10 @@ const Footer = ({ siteTitle }) => (
       <div className={styles.rightcontainer}>
         <nav className={styles.nav}>
           <ul className={styles.list}>
+            <li><Link to="/#">Home</Link></li>
             <li><Link to="/#about">About</Link></li>
             <li><Link to="/#services">Services</Link></li>
             <li><Link to="/#features">Features</Link></li>
-            <li><Link to="/#location">Location</Link></li>
             <li><Link to="/#contact">Contact</Link></li>
           </ul>
         </nav>
