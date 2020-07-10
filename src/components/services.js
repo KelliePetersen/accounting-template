52ada0b39ea81c5ec6226402778079d0e52b50c0
KelliePetersen <kelliepetersen@outlook.com>
2020-07-10 15:49:31 +1000

    make services component margins responsive

--- src/components/services.js
+++ src/components/services.js
@@ -4,25 +4,27 @@ import styles from "./services.module.scss"
 const Services = () => {
   return (
     <section className={styles.services} id='services'>
-      <h2>Our Services.</h2>
-      <div className={styles.grid}>
-        <div>
-          <div className={styles.icon}></div>
-          <h4 className={styles.subheading}>Accounting</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam neque lorem, 
-            vulputate vitae ullamcorper vitae, mattis finibus enim. </p>
-        </div>
-        <div>
-          <div className={styles.icon}></div>
-          <h4 className={styles.subheading}>Auditing</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam neque lorem, 
-            vulputate vitae ullamcorper vitae, mattis finibus enim. </p>
-        </div>
-        <div>
-          <div className={styles.icon}></div>
-          <h4 className={styles.subheading}>Advisors</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam neque lorem, 
-            vulputate vitae ullamcorper vitae, mattis finibus enim. </p>
+      <div className={styles.wrapper}>
+        <h2>Our Services.</h2>
+        <div className={styles.grid}>
+          <div>
+            <div className={styles.icon}></div>
+            <h4 className={styles.subheading}>Accounting</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam neque lorem, 
+              vulputate vitae ullamcorper vitae, mattis finibus enim. </p>
+          </div>
+          <div>
+            <div className={styles.icon}></div>
+            <h4 className={styles.subheading}>Auditing</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam neque lorem, 
+              vulputate vitae ullamcorper vitae, mattis finibus enim. </p>
+          </div>
+          <div>
+            <div className={styles.icon}></div>
+            <h4 className={styles.subheading}>Advisors</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam neque lorem, 
+              vulputate vitae ullamcorper vitae, mattis finibus enim. </p>
+          </div>
         </div>
       </div>
     </section>
