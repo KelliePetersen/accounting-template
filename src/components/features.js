51ecf262d299b74119c00fb639b0202443dc29ec
KelliePetersen <kelliepetersen@outlook.com>
2020-07-14 13:00:31 +1000

    add icons to features section

--- src/components/features.js
+++ src/components/features.js
@@ -7,42 +7,42 @@ const Features = () => (
       <h2>Our specialisations.</h2>
       <div className={styles.grid}>
         <div className={styles.card}>
-          <div className={styles.icon}></div>
+          <div className={`${styles.icon} ${styles.taxation}`}></div>
           <h4 className={styles.subheading}>Taxation</h4>
           <p className={styles.text}>Income Tax, FBT, GST</p>
         </div>
         <div className={styles.card}>
-          <div className={styles.icon}></div>
+          <div className={`${styles.icon} ${styles.investment}`}></div>
           <h4 className={styles.subheading}>Investment</h4>
           <p className={styles.text}>Stock management</p>
         </div>
         <div className={styles.card}>
-          <div className={styles.icon}></div>
+          <div className={`${styles.icon} ${styles.super}`}></div>
           <h4 className={styles.subheading}>Super&shy;annuation</h4>
           <p className={styles.text}>Self managed funds</p>
         </div>
         <div className={styles.card}>
-          <div className={styles.icon}></div>
+          <div className={`${styles.icon} ${styles.estate}`}></div>
           <h4 className={styles.subheading}>Estate Planning</h4>
           <p className={styles.text}>Inheritance & Succession</p>
         </div>
         <div className={styles.card}>
-          <div className={styles.icon}></div>
+          <div className={`${styles.icon} ${styles.startups}`}></div>
           <h4 className={styles.subheading}>Start-Ups</h4>
           <p className={styles.text}>Sole, Partnerships & Companies</p>
         </div>
         <div className={styles.card}>
-          <div className={styles.icon}></div>
+          <div className={`${styles.icon} ${styles.acquisitions}`}></div>
           <h4 className={styles.subheading}>Acquisitions</h4>
           <p className={styles.text}>Smooth change of ownership</p>
         </div>
         <div className={styles.card}>
-          <div className={styles.icon}></div>
+          <div className={`${styles.icon} ${styles.business}`}></div>
           <h4 className={styles.subheading}>Business Advisory</h4>
           <p className={styles.text}>Guidance & Support</p>
         </div>
         <div className={styles.card}>
-          <div className={styles.icon}></div>
+          <div className={`${styles.icon} ${styles.finance}`}></div>
           <h4 className={styles.subheading}>Finance & Insurance</h4>
           <p className={styles.text}>Advice & Guidance</p>
         </div>
